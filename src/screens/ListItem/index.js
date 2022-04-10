5446024b826929185c50ee3e6f46fa12d6fa8ac3
Ezequiel Starecinch <echestare@gmail.com>
2022-04-10 01:47:36 -0300

    The third activity is completed. And comments and extra code were removed

--- src/screens/ListItem/index.js
+++ src/screens/ListItem/index.js
@@ -2,31 +2,21 @@ import React, { Component, useCallback, useEffect, useState } from 'react';
 import { 
     SafeAreaView,
     View, 
-    // Text,
-    Pressable,
-    // Image,
     ImageBackground,
     ActivityIndicator,
     ScrollView,
-    FlatList,
     TouchableOpacity,
 } from 'react-native';
 import { styles } from './styles';
-
 import Collapsible from 'react-native-collapsible';
-
-import { Text, Card, Button, Icon, ListItem, Avatar, Image } from 'react-native-elements';
-
+import { Text, Icon, Image } from 'react-native-elements';
 import { HeaderList } from '../../components/Header';
-
 import { getPokemon, IMG_URL } from '../../api';
 import { getPokemonImgId } from '../../utils';
 
 
 export default Home = (props) => {
     
-    // const BASE_URI = 'https://www.caracteristicas.co/wp-content/uploads/2017/05/universo-1-e1568576142428.jpg';
-
     const { url } = props.route.params.item;
     const [ pokemon, setPokemon ] = useState();
     const [ imgId, setImgId ] = useState();
@@ -36,12 +26,7 @@ export default Home = (props) => {
     let [ url3, setUrl3 ] = useState();
 
 
-
-    
-
-
     useEffect(() => {
-        // if (typeof url === 'string' || url instanceof String){
         if (url?.includes('https')){
             url2 = url;
         }
@@ -59,7 +44,6 @@ export default Home = (props) => {
     
     const loadMore = (prev_next) => {
         if (url3.includes('https')==true){
-        // if (typeof url === 'string' || url instanceof String){
             url2 = url3;
         }
 
@@ -88,13 +72,9 @@ export default Home = (props) => {
 
     return(
         <SafeAreaView style={styles.container}>
-            {/* <ImageBackground style={styles.mainContent} 
-            source={require('../../assets/3.png')}
-            > */}
             <ImageBackground
             resizeMode= "cover"
             source={require('../../assets/images/background.jpg')}
-            // source={{ uri: BASE_URI }}
             Style={[styles.mainContent, {aspectRatio: 1,}]}
             PlaceholderContent={<ActivityIndicator />}
             >
@@ -105,9 +85,7 @@ export default Home = (props) => {
                 <View style={{flex:1}}></View>
                 <View Style={styles.cardContainer}>
                     <View style={[styles.viewContent, {flex:1.3, alignItems: 'center'}]}>
-                    {/* <View style={[styles.viewContent, {backgroundColor:'red', flex:1}]}> */}
                         <Image containerStyle={styles.image} resizeMode='contain' source={{uri: `${IMG_URL}${imgId}.png`}} />
-                        {/* <Image style={styles.image} resizeMode='contain' source={{uri: `${IMG_URL}${imgId}.png`}} /> */}
                         <View style={{flex:3, width:'100%', top:80, flexDirection:'row'}}>
                             <Text style={{color: '#ffffff80', width:'50%', textAlign: 'center'}}>{pokemon_lyric.a}</Text>
                             <Text style={{color: '#ffffff80', width:'50%', textAlign: 'center'}}>{pokemon_lyric.b}</Text>
@@ -115,7 +93,6 @@ export default Home = (props) => {
                         <Text style={styles.shadow}> </Text>
                     </View>
                     <View style={[styles.viewContent, {flex:1}]}>
-                    {/* <View style={[styles.viewContent, {backgroundColor:'blue', flex:1}]}> */}
                         <Text style={[styles.title, {fontSize: 20}]}>Peso: {((pokemon?.weight)/10).toFixed(1)} kg</Text>
                         <Text style={[styles.title, {fontSize: 20}]}>Altura: {((pokemon?.height)/10).toFixed(1)} m</Text>
                         
@@ -156,7 +133,6 @@ export default Home = (props) => {
                 </View>
                 <View style={styles.leftbutton}>
                     <TouchableOpacity
-                        // style= {{margin: '5%'}}
                         onPress={()=>loadMore(false)}
                     >
                         <Icon type='antdesign' name='doubleleft' color='white' style={{size: 90}}/>
@@ -164,7 +140,6 @@ export default Home = (props) => {
                 </View>
                 <View style={styles.rigthbutton}>
                     <TouchableOpacity
-                        // style= {{margin: '5%'}}
                         onPress={()=>loadMore(true)}
                     >
                         <Icon type='antdesign' name='doubleright' color='white' style={{size: 90}}/>
--- src/screens/Lists/index.js
+++ src/screens/Lists/index.js
@@ -3,24 +3,16 @@ import {
     RefreshControl,
     SafeAreaView,
     View, 
-    // Text,
-    Pressable,
     ImageBackground,
-    Image,
     ActivityIndicator,
-    ScrollView,
     FlatList,
     TouchableOpacity,
 } from 'react-native';
 import { styles } from './styles';
-
-import { Text, Card, Button, Icon, ListItem, Avatar } from 'react-native-elements';
-
+import { Icon, ListItem, Avatar } from 'react-native-elements';
 import { HeaderList } from '../../components/Header';
-
 import { getPokemonList, IMG_URL} from '../../api';
 import { getPokemonImgId } from '../../utils';
-// import { TouchableOpacity } from 'react-native-gesture-handler';
 
 
 const wait = (timeout) => {
@@ -33,11 +25,9 @@ export default List = (props) => {
     
     const { leftIcon, rightIcon } = props;
 
-    // const BASE_URI = 'https://www.caracteristicas.co/wp-content/uploads/2017/05/universo-1-e1568576142428.jpg';
 
     const [ generation, setGeneration ] = useState();
     const [ pokemons, setPokemons ] = useState();
-    // const [ previous, setPrevious ] = useState();
     const [ next, setNext ] = useState();
     const [ refreshing, setRefreshing ] = useState(false);
     const [ genName, setgenName ] = useState(false);
@@ -50,25 +40,8 @@ export default List = (props) => {
             setNext(data.next);
         })
     }, [])
-    // useEffect(() => {
-    //     getPokemonList().then(data => {
-    //         setPokemons(data.results);
-            // setPokemons(data.pokemon_entries);//this works with pokemon by pokedex
-    //         setNext(data.next);
-    //     })
-    // }, [])
 
-    // const loadMore_previous = () => {
-    //     setLoadingMore(true);
-    //     getPokemonList(previous).then(data => {
-    //         setPokemons(data.results);
-    //         setPrevious(data.previous);
-    //         setLoadingMore(false);
-    //     })
-    // }
     const loadMore = (prev_next) => {
-        // const pokedex_list = [2,7,15,6,9,12,13,14,21];//this is for POKEDEX: 12, 13 y 14 son kalos central, coastal y mountain
-        // let n = 0?n>8:(n = n+1)
         if (prev_next==true){
             if (counter>7){
                 counter = 1;
@@ -89,50 +62,10 @@ export default List = (props) => {
         getPokemonList(counter).then(data => {
             setgenName(data.name)
             setPokemons(data.pokemon_species);
-            // setPokemons(data.pokemon_entries);//this works with pokemon by pokedex
             setNext(data.next);
             setLoadingMore(false);
         })
     }
-    // const loadMore_next = () => {
-        
-    //     // const pokedex_list = [2,7,15,6,9,12,13,14,21];//this is for POKEDEX: 12, 13 y 14 son kalos central, coastal y mountain
-    //     // let n = 0?n>8:(n = n+1)
-        
-    //     if (counter>7){
-    //         counter = 1;
-    //     }
-    //     else{
-    //         counter = counter+1;
-    //     }
-    //     setLoadingMore(true);
-    //     getPokemonList(counter).then(data => {
-    //         setPokemons(data.pokemon_species);
-    //         // setPokemons(data.pokemon_entries);//this works with pokemon by pokedex
-    //         setNext(data.next);
-    //         setLoadingMore(false);
-    //     })
-    // }
-
-    // const loadMore_next = () => {
-    //     setLoadingMore(true);
-    //     getPokemonList(2, next).then(data => {
-    //         // setPokemons(data.pokemon_entries.pokemon_species);
-    //         setPokemons([...pokemons, ...data.pokemon_entries.pokemon_species]);
-    //         setNext(data.next);
-    //         setLoadingMore(false);
-    //     })
-    // }
-    // const loadMore_next = () => {
-    //     setLoadingMore(true);
-    //     getPokemonList(next).then(data => {
-    //         // setPokemons(data.results);
-    //         setPokemons([...pokemons, ...data.results]);
-    //         setNext(data.next);
-    //         setLoadingMore(false);
-    //     })
-    // }
-
 
     const onRefresh = useCallback(async () => {
         setRefreshing(true);
@@ -140,27 +73,19 @@ export default List = (props) => {
     },[]);
 
     const renderItem = (item, index) =>{
-        // const imgID = getPokemonImgId(JSON.stringify(item.id))
-        // const imgID = getPokemonImgId(JSON.stringify(index+1))
-        // const path = item.entry_number;
         const path = item.url.split('/')
 
-        // const imgID = getPokemonImgId(path.toString())
         const imgID = getPokemonImgId(path[6])
 
         return (
             <ListItem key={index} bottomDivider style={styles.ListItem}>
             <TouchableOpacity 
             onPress={()=>props.navigation.navigate('ListItem', item={item})}
-            // style={styles.listContent}
             >
                 <Avatar size='large' source={{uri: `${IMG_URL}${imgID}.png`}} />
                 <ListItem.Content>
-                    {/* <ListItem.Title> {imgID} - {item.name.toUpperCase()}</ListItem.Title> */}
                     <ListItem.Title> {imgID}</ListItem.Title>
                     <ListItem.Title>{item.name.toUpperCase()}</ListItem.Title>
-                    {/* <ListItem.Title>{item.pokemon_species.name}</ListItem.Title> */}
-                    {/* <ListItem.Subtitle>{item.id}</ListItem.Subtitle> */}
                 </ListItem.Content>
             </TouchableOpacity>
           </ListItem>
@@ -169,37 +94,18 @@ export default List = (props) => {
     
     return(
         <SafeAreaView style={styles.container}>
-            {/* <ImageBackground style={styles.mainContent} 
-            source={require('../../assets/3.png')}
-            > */}
             <ImageBackground
             resizeMode= "cover"
             source={require('../../assets/images/background.jpg')}
-            // source={{ uri: BASE_URI }}
             Style={[styles.mainContent, {aspectRatio: 1,}]}
             PlaceholderContent={<ActivityIndicator />}
             >
-                {/* <HeaderList /> */}
                 <HeaderList title={genName} />
-                {/* <HeaderList title={genName?.toUpperCase()} /> */}
-
-                {/* <View style={styles.cardContainer}> */}
-                    {/* <Text style={[styles.title, {fontSize: 40}]}>PANTALLA DE</Text>
-                    <Text style={[styles.title, {fontSize: 40}]}>"LISTA"</Text> */}
-                {/* </View> */}
-                {/* <View>
-
-                </View> */}
                 <View style = {styles.list}>
-                    {/* <ScrollView style={styles.list}>
-                        {pokemons && pokemons.map((item, index) => renderItem(item, index))}
-                    </ScrollView> */}
                     <FlatList
-                        // data={pokemons}
                         data={pokemons?.sort(function(a, b){
                             return (Number(a.url.split('/')[6]) < Number(b.url.split('/')[6])) ? -1 : (Number(a.url.split('/')[6]) > Number(b.url.split('/')[6])) ? 1 : 0;
                            })}
-                        // bouces = {false}
                         numColumns={2}
                         columnWrapperStyle={{justifyContent:'space-between', alignContent: 'center'}}
                         ItemSeparatorComponent={
@@ -207,33 +113,16 @@ export default List = (props) => {
                         }
                         renderItem={(item, index) => renderItem(item.item, index)}
                         keyExtractor={(item, index) => index}
-                        // style = {styles.list}
                         refreshControl={
                             <RefreshControl
                             refreshing={refreshing}
                             onRefresh={() => onRefresh()}
                             />
                         }
-                        // ListHeaderComponent={
-                        //     loadingMore?
-                        //     (<ActivityIndicator />):
-                        //     (<Button title='Cargar más...' onPress={() => loadMore_next()}/>)
-                        // }
-                        // ListHeaderComponent={
-                        //     loadingMore?
-                        //     (<ActivityIndicator />):
-                        //     (<Button title='Cargar más...' onPress={() => loadMore_previous()}/>)
-                        // }
-                        // ListFooterComponent={
-                        //     loadingMore?
-                        //     (<ActivityIndicator />):
-                        //     (<Button title='Cargar más...' onPress={() => loadMore_next()}/>)
-                        // }
                     />
                 </View>
                 <View style={styles.leftbutton}>
                     <TouchableOpacity
-                        // style= {{margin: '5%'}}
                         onPress={()=>loadMore(false)}
                     >
                         <Icon type='antdesign' name='doubleleft' color='white' style={{size: 90}}/>
@@ -242,7 +131,6 @@ export default List = (props) => {
                 <View style={styles.rigthbutton}>
                     <TouchableOpacity
                         // style= {{margin: '5%'}}
-                        onPress={()=>loadMore(true)}
                     >
                         <Icon type='antdesign' name='doubleright' color='white' style={{size: 90}}/>
                     </TouchableOpacity>
